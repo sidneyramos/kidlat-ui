c48864680d6e57b7d63c1ae4a47562fa2ef0ca05
sidneyramos <sidney.ramos95@gmail.com>
2020-06-28 01:35:09 +1000

    Added core packages

---
+++ packages/core/components/Button/Button.tsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+export default () => <button>I SHOULD BE HERE</button>;
---
+++ packages/core/lib/index.esm.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+var Button = () => React.createElement("button", null, "I SHOULD BE HERE");
+
+export { Button };
